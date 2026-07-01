3bbb3b716208f75a92da4a707bd7ba2aafd21f29
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-07-01 18:07:12 +0500

    React/Vite, basic components and file stucture setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Live from "./components/Live";
+import Sidebar from "./components/Sidebar";
+import Converter from "./components/Converter";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Live />
+      <Sidebar />
+      <Converter />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Converter.jsx
@@ -0,0 +1,5 @@
+function Converter() {
+  return <h1>This is Converter</h1>;
+}
+
+export default Converter;
