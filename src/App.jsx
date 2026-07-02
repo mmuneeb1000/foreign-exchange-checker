f8cdb0c27753d5d6096dfc806d0b950990b0da3d
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-07-03 00:31:46 +0500

    rename to tabs and visibility on desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Live from "./components/Live";
-import ToggleBar from "./components/ToggleBar";
+import TabsBar from "./components/TabsBar";
 import Converter from "./components/Converter";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Live />
       <main className="px-4 py-8">
         <Converter />
-        <ToggleBar />
+        <TabsBar />
       </main>
       <Footer />
     </>
--- src/components/TabsBar.jsx
+++ src/components/TabsBar.jsx
@@ -3,14 +3,14 @@ import History from "./ToggleMenu/History";
 import Log from "./ToggleMenu/Log";
 import Favorites from "./ToggleMenu/Favorites";
 
-function ToggleBar() {
+function TabsBar() {
   return (
     <section>
-      <div>
+      <div className="lg:hidden">
         <select
           name="toggle"
           id="toggle"
-          className="w-full bg-neutral-700 border 
+          className=" w-full bg-neutral-700 border 
           border-neutral-300 rounded-xl px-2 py-2"
         >
           <option value="History">History</option>
@@ -19,8 +19,16 @@ function ToggleBar() {
           <option value="Log">Log</option>
         </select>
       </div>
+      <div className="hidden lg:flex">
+        <ul className="flex gap-4">
+          <li>History </li>
+          <li>Compare</li>
+          <li>Favorites</li>
+          <li>Log</li>
+        </ul>
+      </div>
     </section>
   );
 }
 
-export default ToggleBar;
+export default TabsBar;
